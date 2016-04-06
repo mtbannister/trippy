9496bf731f0c34af58c16ec177d2ba3d9e3f8f45
mtbannister <micheleb@uvic.ca>
2016-04-05 18:36:51 -0700

    PEP8 the init

--- trippy/__init__.py
+++ trippy/__init__.py
@@ -1,5 +1,5 @@
+__version__ = '0.3'
+__author__ = 'Wesley Fraser (github: fraserw)'
+__all__ = ['psf', 'pill', 'bgFinder', 'utils', 'psfStarChooser', 'MCMCfit', 'scamp']
 
-__version__='0.3'
-__author__='Wesley Fraser (github: fraserw)'
-__all__=['psf','pill','bgFinder','utils','psfStarChooser','MCMCfit','scamp']
-import psf,pill,bgFinder
+import psf, pill, bgFinder
